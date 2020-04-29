e441f30824c8566fb998b2e6e36c5a0db9b7e96d
Harriet Melesi <harrietmelesi@harriets-air.lan>
2020-04-29 15:15:43 +0100

    15 can toggle habits

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,28 @@
 import 'package:flutter/material.dart';
+// import 'package:needy_new/MyHabits.dart';
+import 'package:needy_new/Welcome.dart';
 // import 'package:needy_new/NewHabit.dart';
-import 'package:needy_new/authentication.dart';
-// import 'package:needy_pet/Welcome.dart';
-// import 'package:needy_pet/SignIn.dart';
-import 'package:needy_new/RootPage.dart';
+// import 'package:needy_new/authentication.dart';
+
+// import 'package:needy_new/SignIn.dart';
+// import 'package:needy_new/RootPage.dart';
 
 void main() {
   return runApp(MaterialApp(
-    home: Scaffold(
-        backgroundColor: Colors.purple,
-        appBar: AppBar(
-          title: Text(
-            'KEEPER',
-            style: TextStyle(
-              fontFamily: 'PressStart2P',
-              color: Colors.yellow,
-            ),
-          ),
-          backgroundColor: Colors.purple,
+      home: Scaffold(
+    backgroundColor: Colors.purple,
+    appBar: AppBar(
+      title: Text(
+        'KEEPER',
+        style: TextStyle(
+          fontFamily: 'PressStart2P',
+          color: Colors.yellow,
         ),
-        // body: NewHabit()
-        body: new RootPage(auth: new Auth())),
-  ));
+      ),
+      backgroundColor: Colors.purple,
+    ),
+    // body: NewHabit()
+    // body: MyHabits(),
+    body: HomePage(),
+  )));
 }
