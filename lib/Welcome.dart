233b84837045adbcac170d1537b47672f2ccb29c
Harriet Melesi <59828008+HMelesi@users.noreply.github.com>
2020-05-01 10:31:01 +0100

    [17] can navigate using drawer (#15)
    Co-authored-by: Harriet Melesi <harrietmelesi@harriets-air.lan>

--- lib/Welcome.dart
+++ lib/Welcome.dart
@@ -3,6 +3,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:needy_new/GoalSetter.dart';
 // import 'package:needy_new/MyHabits.dart';
 import 'package:needy_new/authentication.dart';
+import 'package:needy_new/MyScaffold.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({Key key, this.auth, this.userId, this.logoutCallback, this.name})
@@ -14,7 +15,8 @@ class HomePage extends StatefulWidget {
   final String name;
 
   @override
-  State<StatefulWidget> createState() => new _HomePageState(userId: userId);
+  State<StatefulWidget> createState() => new _HomePageState(
+      userId: userId, name: name, logoutCallback: logoutCallback);
 }
 
 class _HomePageState extends State<HomePage> {
@@ -24,71 +26,77 @@ class _HomePageState extends State<HomePage> {
   final BaseAuth auth;
   final VoidCallback logoutCallback;
   final String userId;
-  String name;
+  final String name;
 
   @override
   Widget build(BuildContext context) {
-    print(userId);
-    return Column(
-      children: <Widget>[
-        (userId == null)
-            ? Text('no userid')
-            : new StreamBuilder(
-                stream: Firestore.instance
-                    .collection('users')
-                    .document(userId)
-                    .snapshots(),
-                builder: (context, snapshot) {
-                  if (!snapshot.hasData) {
-                    return new Text("Loading");
-                  }
-                  var userDocument = snapshot.data;
-                  name = userDocument["username"];
-                  final goals = userDocument['goals'];
-                  return Column(
-                    children: <Widget>[
-                      new Text('hi $name, welcome to the app!'),
-                      (goals == null)
-                          ? Column(
-                              children: <Widget>[
-                                Text(
-                                    'hmmm it looks like you have no goals at the moment, would you like to set one up?'),
-                                RaisedButton(
-                                  textColor: Colors.white,
-                                  color: Colors.pink,
-                                  child: Text(
-                                    'Create a new goal',
-                                    style: TextStyle(
-                                      fontFamily: 'PressStart2P',
-                                      color: Colors.yellow,
+    print('welcome: $userId $name');
+    return MyScaffold(
+      logoutCallback: logoutCallback,
+      name: name,
+      userId: userId,
+      body: Column(
+        children: <Widget>[
+          (userId == null)
+              ? Text('you idiot you have not passed the userid')
+              : new StreamBuilder(
+                  stream: Firestore.instance
+                      .collection('users')
+                      .document(userId)
+                      .snapshots(),
+                  builder: (context, snapshot) {
+                    if (!snapshot.hasData) {
+                      return new Text("Loading");
+                    }
+                    var userDocument = snapshot.data;
+                    // name = userDocument["username"];
+                    final goals = userDocument['goals'];
+                    return Column(
+                      children: <Widget>[
+                        new Text('hi $name, welcome to the app!'),
+                        (goals == null)
+                            ? Column(
+                                children: <Widget>[
+                                  Text(
+                                      'hmmm it looks like you have no goals at the moment, would you like to set one up?'),
+                                  RaisedButton(
+                                    textColor: Colors.white,
+                                    color: Colors.pink,
+                                    child: Text(
+                                      'Create a new goal',
+                                      style: TextStyle(
+                                        fontFamily: 'PressStart2P',
+                                        color: Colors.yellow,
+                                      ),
+
                                     ),
-                                  ),
-                                  onPressed: () {
-                                    toGoalSetter(context);
-                                  },
-                                )
-                              ],
-                            )
-                          : Text('here are your goals'),
-                    ],
-                  );
-                },
+                                    onPressed: () {
+                                      toGoalSetter(context);
+                                    },
+                                  )
+                                ],
+                              )
+                            : Text('here are your goals'),
+                      ],
+                    );
+                  },
+                ),
+          RaisedButton(
+            textColor: Colors.white,
+            color: Colors.pink,
+            child: Text(
+              'Logout',
+              style: TextStyle(
+                fontFamily: 'PressStart2P',
+                color: Colors.yellow,
               ),
-        RaisedButton(
-          textColor: Colors.white,
-          color: Colors.pink,
-          child: Text(
-            'Logout',
-            style: TextStyle(
-              fontFamily: 'PressStart2P',
-              color: Colors.yellow,
             ),
-          ),
-          onPressed: () {
-            widget.logoutCallback();
-          },
-        )
-      ],
+            onPressed: () {
+              widget.logoutCallback();
+            },
+          )
+        ],
+      ),
     );
   }
 
