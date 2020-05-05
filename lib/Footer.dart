ed032daa947afd1676ca713a825042291d0061ea
Harriet Melesi <59828008+HMelesi@users.noreply.github.com>
2020-05-05 11:05:29 +0100

    [19] footer fixed (#33)
    Co-authored-by: Harriet Melesi <harrietmelesi@harriets-air.lan>

--- lib/Footer.dart
+++ lib/Footer.dart
@@ -6,11 +6,6 @@ class Footer extends StatelessWidget {
 
   final String userId;
 
-  // Stream<List<String>> loadData() async* {
-  //   await Future.delayed(Duration(seconds: 3));
-  //   yield List.generate(10, (index) => "Index $index");
-  // }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,31 +17,55 @@ class Footer extends StatelessWidget {
             .snapshots(),
         builder: (context, snapshot) {
           final int goalsCount = snapshot.data.documents.length;
+          if (!snapshot.hasData) return new Text('loading...');
           if (snapshot.hasData) {
-            return CustomScrollView(
-              // shrinkWrap: true,
-              scrollDirection: Axis.horizontal,
-              slivers: [
-                SliverList(
-                  delegate: SliverChildBuilderDelegate(
-                    (context, index) {
-                      for (int ind = 0; ind < goalsCount; ind++) {
-                        final DocumentSnapshot goal =
-                            snapshot.data.documents[ind];
-                        return Container(
-                          height: 30.0,
-                          width: 200.0,
-                          child: ListTile(
-                            title: Text(goal['petName']),
-                            trailing: Text(goal['petType']),
-                          ),
-                        );
-                      }
-                      return null;
-                    },
-                    childCount: 3,
+            return Column(
+              children: <Widget>[
+                Container(
+                    height: 20.0,
+                    width: 600.0,
+                    child: Row(
+                      children: <Widget>[
+                        Icon(Icons.arrow_left),
+                        Text('play with your pets'),
+                        Icon(Icons.arrow_right),
+                      ],
+                    )),
+                Container(
+                  height: 50.0,
+                  width: 600.0,
+                  child: CustomScrollView(
+                    // shrinkWrap: true,
+                    scrollDirection: Axis.horizontal,
+                    slivers: [
+                      SliverList(
+                        delegate: SliverChildBuilderDelegate(
+                            (BuildContext context, int index) {
+                          if (index < goalsCount) {
+                            DocumentSnapshot goal =
+                                snapshot.data.documents[index];
+                            String petType = goal['petType'];
+                            return Container(
+                                height: 30.0,
+                                width: 200.0,
+                                child: ListTile(
+                                  title: Text(goal['petName'],
+                                      style: TextStyle(
+                                          color: (goal['petHealth'] == 0)
+                                              ? Colors.red
+                                              : (goal['petHealth'] > 5)
+                                                  ? Colors.green
+                                                  : Colors.orange)),
+                                  trailing:
+                                      Image.asset('images/pixil-$petType.png'),
+                                ));
+                          }
+                          return null;
+                        }),
+                      )
+                    ],
                   ),
-                )
+                ),
               ],
             );
           } else {
@@ -57,104 +76,3 @@ class Footer extends StatelessWidget {
     );
   }
 }
-
-// class Footer extends StatelessWidget {
-//   Footer({this.userId});
-
-//   final String userId;
-
-//   @override
-//   Widget build(BuildContext context) {
-//     print('footer: $userId');
-//     return CustomScrollView(
-//       shrinkWrap: true,
-//       scrollDirection: Axis.horizontal,
-//       slivers: StreamBuilder<QuerySnapshot>(
-//         stream: Firestore.instance.collection('users')
-//             .document(userId)
-//             .collection('goals')
-//             .snapshots(),
-//         builder: (context, snapshot) {
-//           if (!snapshot.hasData) return LinearProgressIndicator();
-//           return _buildGoalList(context, snapshot.data.documents);
-//         },
-//       )
-//     );
-//   }
-// }
-
-// Widget _buildGoalList(
-//       BuildContext context, List<DocumentSnapshot> snapshot) {
-//     return SliverList(
-//           delegate: SliverChildListDelegate(
-//       // padding: const EdgeInsets.only(top: 20.0),
-//       // children:
-//           snapshot.map((data) => _buildGoalListItem(context, data)).toList(),
-//     ));
-// }
-
-// Widget _buildGoalListItem(BuildContext context, DocumentSnapshot data) {
-//     final goalrecord = GoalRecord.fromSnapshot(data);
-
-//     return Padding(
-//       key: ValueKey(goalrecord.pet),
-//       padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-//       child: Container(
-//         decoration: BoxDecoration(color: Colors.purple),
-//         child: ListTile(
-//           title: Text(goalrecord.pet,
-//               style: TextStyle(
-//                 fontFamily: 'PressStart2P',
-//                 // color: (habitrecord.outstanding == true)
-//                 //     ? Colors.red
-//                 //     : Colors.grey,
-//               )),
-//           // onTap: () => habitrecord.reference
-//           //     .updateData({'outstanding': !habitrecord.outstanding}),
-//           // trailing: Icon(
-//           //   (habitrecord.outstanding == true)
-//           //       ? Icons.beach_access
-//           //       : Icons.check,
-//             // color: Colors.green,
-//           ),
-//         ),
-//       );
-
-//   }
-
-// class GoalRecord {
-//   // final String goal;
-//   final String pet;
-//   final String petname;
-
-//   final DocumentReference reference;
-
-//   GoalRecord.fromMap(Map<String, dynamic> map, {this.reference})
-//       : assert(map['pet'] != null),
-//         assert(map['petname'] != null),
-//         pet = map['pet'],
-//         petname = map['petname'];
-
-//   GoalRecord.fromSnapshot(DocumentSnapshot snapshot)
-//       : this.fromMap(snapshot.data, reference: snapshot.reference);
-
-//   @override
-//   String toString() => "GoalRecord<$pet$petname>";
-// }
-
-//       // <Widget>[
-//       //   SliverList(
-//       //     delegate: SliverChildListDelegate(
-//       //       [
-//       //         Text("Header 1"),
-//       //         Text("Header 2"),
-//       //         Text("Header 3"),
-//       //         Text("Header 4"),
-//       //         Text("Header 5"),
-//       //         Text("Header 6"),
-//       //         Text("Header 7"),
-//       //         Text("Header 8"),
-//       //       ],
-//       //     ),
-//       //   ),
-//       // ],
