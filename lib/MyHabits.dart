bcc38418710667432afb7a387982036b52344c5e
blueca <nickoldacre@outlook.com>
2020-05-07 15:43:00 +0100

    add scroll fade to my habits

--- lib/MyHabits.dart
+++ lib/MyHabits.dart
@@ -5,6 +5,7 @@ import 'package:needy_new/MyScaffold.dart';
 import 'package:needy_new/NewHabit.dart';
 import 'package:needy_new/Summary.dart';
 import 'package:intl/intl.dart';
+import 'package:fading_edge_scrollview/fading_edge_scrollview.dart';
 
 class MyHabits extends StatefulWidget {
   MyHabits({Key key, this.userId, this.name, this.goalName, this.endDate})
@@ -36,103 +37,101 @@ class _MyHabits extends State<MyHabits> {
     return MyScaffold(
       userId: userId,
       name: name,
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: MarqueeWidget(
-                direction: Axis.horizontal,
-                child: Text(
-                  goalName,
-                  style: TextStyle(
-                    fontFamily: 'PressStart2P',
-                    fontSize: 24,
-                  ),
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: MarqueeWidget(
+              direction: Axis.horizontal,
+              child: Text(
+                goalName,
+                style: TextStyle(
+                  fontFamily: 'PressStart2P',
+                  fontSize: 24,
                 ),
               ),
             ),
-            Text(
-              'This goal will end on ${DateFormat.yMMMMEEEEd().format(endDate.toDate())}',
+          ),
+          Text(
+            'This goal will end on ${DateFormat.yMMMMEEEEd().format(endDate.toDate())}',
+            style: TextStyle(
+              fontFamily: 'Pixelar',
+              fontSize: 18,
+              color: Colors.grey[800],
+            ),
+          ),
+          RaisedButton(
+            textColor: Colors.white,
+            color: Colors.pink,
+            child: Text(
+              'Create a new habit',
               style: TextStyle(
-                fontFamily: 'Pixelar',
-                fontSize: 18,
-                color: Colors.grey[800],
+                fontFamily: 'PressStart2P',
+                color: Colors.yellow,
               ),
             ),
-            RaisedButton(
-              textColor: Colors.white,
-              color: Colors.pink,
-              child: Text(
-                'Create a new habit',
-                style: TextStyle(
-                  fontFamily: 'PressStart2P',
-                  color: Colors.yellow,
-                ),
+            onPressed: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (BuildContext context) => NewHabit(
+                        userId: userId, name: name, goalName: goalName),
+                  ));
+            },
+          ),
+          RaisedButton(
+            textColor: Colors.white,
+            color: Colors.pink,
+            child: Text(
+              'View summary',
+              style: TextStyle(
+                fontFamily: 'PressStart2P',
+                color: Colors.yellow,
               ),
-              onPressed: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (BuildContext context) => NewHabit(
-                          userId: userId, name: name, goalName: goalName),
-                    ));
+            ),
+            onPressed: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (BuildContext context) =>
+                        Summary(userId: userId, goalName: goalName),
+                  ));
+            },
+          ),
+          Expanded(
+            child: StreamBuilder<QuerySnapshot>(
+              stream: Firestore.instance
+                  .collection('users')
+                  .document(userId)
+                  .collection('goals')
+                  .document(goalName)
+                  .collection('habits')
+                  .orderBy('outstanding', descending: true)
+                  .snapshots(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  print('there are no habits here we need to create a ternary');
+                  return LinearProgressIndicator();
+                }
+                return _buildHabitList(context, snapshot.data.documents);
               },
             ),
-            RaisedButton(
-              textColor: Colors.white,
-              color: Colors.pink,
-              child: Text(
-                'View summary',
-                style: TextStyle(
-                  fontFamily: 'PressStart2P',
-                  color: Colors.yellow,
-                ),
-              ),
-              onPressed: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (BuildContext context) =>
-                          Summary(userId: userId, goalName: goalName),
-                    ));
-              },
-            ),
-            Container(
-              height: 500.0,
-              width: 600.0,
-              child: StreamBuilder<QuerySnapshot>(
-                stream: Firestore.instance
-                    .collection('users')
-                    .document(userId)
-                    .collection('goals')
-                    .document(goalName)
-                    .collection('habits')
-                    .orderBy('outstanding', descending: true)
-                    .snapshots(),
-                builder: (context, snapshot) {
-                  if (!snapshot.hasData) {
-                    print(
-                        'there are no habits here we need to create a ternary');
-                    return LinearProgressIndicator();
-                  }
-                  // print(Firestore.instance.collection('new_habit').snapshots());
-                  return _buildHabitList(context, snapshot.data.documents);
-                },
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
 
   Widget _buildHabitList(
       BuildContext context, List<DocumentSnapshot> snapshot) {
-    return ListView(
-      padding: const EdgeInsets.only(top: 20.0),
-      children:
-          snapshot.map((data) => _buildHabitListItem(context, data)).toList(),
+    final _controller = ScrollController();
+    return FadingEdgeScrollView.fromScrollView(
+      child: ListView(
+        controller: _controller,
+        padding: const EdgeInsets.only(top: 20.0),
+        children:
+            snapshot.map((data) => _buildHabitListItem(context, data)).toList(),
+      ),
     );
   }
 
