6efdce98d151fa8381b6c8aa95e9c7b2d281b32f
James Cowell <james.cowell28@gmail.com>
2020-05-09 18:04:32 +0100

    added catgif and user since date to user profile

--- lib/UserProfile.dart
+++ lib/UserProfile.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:needy_new/MyScaffold.dart';
 import 'package:needy_new/authentication.dart';
+import 'package:intl/intl.dart';
 
 class UserProfile extends StatefulWidget {
   UserProfile({Key key, this.auth, this.userId, this.name}) : super(key: key);
@@ -23,10 +24,12 @@ class _UserProfileState extends State<UserProfile> {
   final String userId;
   final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
   String name;
+  Timestamp userSince;
 
   @override
   Widget build(BuildContext context) {
     getUsername(userId);
+    getUserSince(userId);
     print('user profile for $userId');
     updateToken();
 
@@ -44,69 +47,85 @@ class _UserProfileState extends State<UserProfile> {
                   .where('expired', isEqualTo: false)
                   .snapshots(),
               builder: (context, snapshot) {
-                return SingleChildScrollView(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      Padding(
-                        padding: const EdgeInsets.all(20.0),
-                        child: Text(
-                          '$name',
-                          style: TextStyle(
-                            fontFamily: 'PressStart2P',
-                            fontSize: 24,
+                return Center(
+                  child: SingleChildScrollView(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        Padding(
+                          padding: EdgeInsets.fromLTRB(0.0, 30.0, 0.0, 0.0),
+                          child: CircleAvatar(
+                            backgroundColor: Colors.transparent,
+                            radius: 48.0,
+                            child: Image.asset('images/catgif.gif'),
                           ),
                         ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text(
-                          'User since:',
-                          style: TextStyle(
-                            fontFamily: 'Pixelar',
-                            fontSize: 26,
-                            color: Colors.grey[800],
+                        Padding(
+                          padding: const EdgeInsets.all(20.0),
+                          child: Text(
+                            '$name',
+                            style: TextStyle(
+                              fontFamily: 'PressStart2P',
+                              fontSize: 24,
+                            ),
                           ),
                         ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text(
-                          'Total number of goals:',
-                          style: TextStyle(
-                            fontFamily: 'Pixelar',
-                            fontSize: 26,
-                            color: Colors.grey[800],
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            'User since: ${DateFormat.yMMMMEEEEd().format(userSince.toDate())}',
+                            style: TextStyle(
+                              fontFamily: 'Pixelar',
+                              fontSize: 26,
+                              color: Colors.grey[800],
+                            ),
                           ),
                         ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text(
-                          'Goals completed:',
-                          style: TextStyle(
-                            fontFamily: 'Pixelar',
-                            fontSize: 26,
-                            color: Colors.grey[800],
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            'Total number of goals:',
+                            style: TextStyle(
+                              fontFamily: 'Pixelar',
+                              fontSize: 26,
+                              color: Colors.grey[800],
+                            ),
                           ),
                         ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(20.0),
-                        child: Text(
-                          'Badges',
-                          style: TextStyle(
-                            fontFamily: 'PressStart2P',
-                            fontSize: 22,
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            'Goals completed:',
+                            style: TextStyle(
+                              fontFamily: 'Pixelar',
+                              fontSize: 26,
+                              color: Colors.grey[800],
+                            ),
                           ),
                         ),
-                      ),
-                    ],
+                        Padding(
+                          padding: const EdgeInsets.all(20.0),
+                          child: Text(
+                            'Badges',
+                            style: TextStyle(
+                              fontFamily: 'PressStart2P',
+                              fontSize: 22,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 );
               }),
     );
+  }
+
+  Future getUserSince(userId) async {
+    Firestore.instance.collection('users').document(userId).get().then((res) {
+      userSince = (res['userSince']);
+    });
   }
 
   Future getUsername(userId) async {
